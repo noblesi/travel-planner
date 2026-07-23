9faa1c59abda19b33944b91383d7cd5a6c290c49
chaedae <enemy@hanmail.net>
2026-07-23 14:43:02 +0900

    회원가입 페이지 디자인 작성

--- frontend/src/views/loginView/JoinView.vue
+++ frontend/src/views/loginView/JoinView.vue
@@ -1,8 +1,16 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const email = ref('')
 const password = ref('')
+const passwordCheck = ref('')
+
+
+
+const isPasswordMatched = computed(() => {
+  if (!passwordCheck.value) return true; 
+  return password.value === passwordCheck.value;
+});
 
 const handleContinue = () => {
   if (!email.value) {
@@ -19,10 +27,17 @@ const handleContinue = () => {
     alert('비밀번호는 10자 이상 입력해주셔야 합니다.')
     return
   }
+  
+  if (!isPasswordMatched.value) {
+    alert('비밀번호가 일치하지 않습니다.');
+    return;
+  }
 
   alert("로그인 유효성 검사")
 
 }
+
+
 
 </script>
 
@@ -49,7 +64,7 @@ const handleContinue = () => {
           />
           <label class="input-label">비밀번호</label>
           <input 
-            type="password" 
+            type="password"
             v-model="password" 
             placeholder="비밀번호를 입력해주세요." 
             required
@@ -57,10 +72,22 @@ const handleContinue = () => {
           <label class="input-label">비밀번호 확인</label>
           <input 
             type="password" 
-            v-model="password" 
+            v-model="passwordCheck"
             placeholder="비밀번호를 입력해주세요." 
             required
           />
+          <br/>
+           <!-- 비밀번호 확인란에 입력이 시작되었을 때만 메시지 노출 -->
+          <div v-if="passwordCheck" style="margin-top: 5px;" class="passwordMatchedDiv">
+            <!-- 일치하지 않을 때 -->
+            <span v-show="!isPasswordMatched" style="color: #ff4d4d; font-size: 14px;">
+              ❌ 비밀번호가 맞지 않습니다.
+            </span>
+            <!-- 일치할 때 -->
+            <span v-show="isPasswordMatched" style="color: #222573; font-size: 14px;">
+              ✅ 비밀번호가 일치합니다.
+            </span>
+          </div>
         </div>
         
         <button type="submit" class="btn-submit">가입하기</button>
@@ -68,13 +95,36 @@ const handleContinue = () => {
       <div class="divider">
         <span class="divider-text">또는 다음으로 계속하기</span>
       </div>
-
+      <div class="social-grid-triple">
+        <div class="gsi-material-button">
+            <div class="gsi-material-button-state"></div>
+            <div class="gsi-material-button-content-wrapper">
+                <div class="gsi-material-button-icon">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" xmlns:xlink="http://www.w3.org/1999/xlink" style="display: block;">
+                    <path fill="#EA4335" d="M24 9.5c3.54 0 6.71 1.22 9.21 3.6l6.85-6.85C35.9 2.38 30.47 0 24 0 14.62 0 6.51 5.38 2.56 13.22l7.98 6.19C12.43 13.72 17.74 9.5 24 9.5z"></path>
+                    <path fill="#4285F4" d="M46.98 24.55c0-1.57-.15-3.09-.38-4.55H24v9.02h12.94c-.58 2.96-2.26 5.48-4.78 7.18l7.73 6c4.51-4.18 7.09-10.36 7.09-17.65z"></path>
+                    <path fill="#FBBC05" d="M10.53 28.59c-.48-1.45-.76-2.99-.76-4.59s.27-3.14.76-4.59l-7.98-6.19C.92 16.46 0 20.12 0 24c0 3.88.92 7.54 2.56 10.78l7.97-6.19z"></path>
+                    <path fill="#34A853" d="M24 48c6.48 0 11.93-2.13 15.89-5.81l-7.73-6c-2.15 1.45-4.92 2.3-8.16 2.3-6.26 0-11.57-4.22-13.47-9.91l-7.98 6.19C6.51 42.62 14.62 48 24 48z"></path>
+                    <path fill="none" d="M0 0h48v48H0z"></path>
+                </svg>
+                </div>
+                <span class="gsi-material-button-contents">Sign in with Google</span>
+                <span style="display: none;">Sign in with Google</span>
+            </div>
+        </div>
+        
+      </div>
 
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.passwordMatchedDiv{
+  height: 14px;
+  text-align: center;
+
+}
 // 전체 배경 배치
 .login-container {
   display: flex;
@@ -159,7 +209,7 @@ const handleContinue = () => {
 
 // 메인 '계속' 파란색 버튼
 .btn-submit {
-  margin-top: 40px;
+  margin-top: 20px;
   margin-left: 20px;
   width: 90%;
   height: 44px;
@@ -181,7 +231,7 @@ const handleContinue = () => {
 .divider {
   display: flex;
   align-items: center;
-  margin: 32px 0 20px 0;
+  margin: 15px 0 20px 0;
 
   &::before, &::after {
     content: '';
@@ -190,7 +240,7 @@ const handleContinue = () => {
   }
 
   .divider-text {
-    padding: 0 16px;
+    padding: 0 10px;
     font-size: 12px;
     color: #9b9b9b;
   }
@@ -277,6 +327,114 @@ const handleContinue = () => {
       color: #9b9b9b;
     }
   }
+}
+//구글 버튼 디자인
+
+//google 버튼 디자인
+.gsi-material-button {
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  -webkit-appearance: none;
+  background-color: #f2f2f2;
+  background-image: none;
+  border: none;
+  -webkit-border-radius: 20px;
+  border-radius: 20px;
+  -webkit-box-sizing: border-box;
+  box-sizing: border-box;
+  color: #1f1f1f;
+  cursor: pointer;
+  font-family: 'Roboto', arial, sans-serif;
+  font-size: 14px;
+  height: 40px;
+  letter-spacing: 0.25px;
+  outline: none;
+  overflow: hidden;
+  padding: 0 12px;
+  position: relative;
+  text-align: center;
+  -webkit-transition: background-color .218s, border-color .218s, box-shadow .218s;
+  transition: background-color .218s, border-color .218s, box-shadow .218s;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: 360px;
+  margin-left: 20px;
+}
+
+.gsi-material-button .gsi-material-button-icon {
+  height: 20px;
+  margin-right: 10px;
+  min-width: 20px;
+  width: 20px;
+}
+
+.gsi-material-button .gsi-material-button-content-wrapper {
+  -webkit-align-items: center;
+  align-items: center;
+  display: flex;
+  -webkit-flex-direction: row;
+  flex-direction: row;
+  -webkit-flex-wrap: nowrap;
+  flex-wrap: nowrap;
+  height: 100%;
+  justify-content: space-between;
+  position: relative;
+  width: 100%;
+}
+
+.gsi-material-button .gsi-material-button-contents {
+  -webkit-flex-grow: 1;
+  flex-grow: 1;
+  font-family: 'Roboto', arial, sans-serif;
+  font-weight: 500;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  vertical-align: top;
+}
+
+.gsi-material-button .gsi-material-button-state {
+  -webkit-transition: opacity .218s;
+  transition: opacity .218s;
+  bottom: 0;
+  left: 0;
+  opacity: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+.gsi-material-button:disabled {
+  cursor: default;
+  background-color: #ffffff61;
+}
+
+.gsi-material-button:disabled .gsi-material-button-state {
+  background-color: #1f1f1f1f;
+}
+
+.gsi-material-button:disabled .gsi-material-button-contents {
+  opacity: 38%;
+}
+
+.gsi-material-button:disabled .gsi-material-button-icon {
+  opacity: 38%;
+}
+
+.gsi-material-button:not(:disabled):active .gsi-material-button-state, 
+.gsi-material-button:not(:disabled):focus .gsi-material-button-state {
+  background-color: #001d35;
+  opacity: 12%;
+}
+
+.gsi-material-button:not(:disabled):hover {
+  -webkit-box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .30), 0 1px 3px 1px rgba(60, 64, 67, .15);
+  box-shadow: 0 1px 2px 0 rgba(60, 64, 67, .30), 0 1px 3px 1px rgba(60, 64, 67, .15);
+}
+
+.gsi-material-button:not(:disabled):hover .gsi-material-button-state {
+  background-color: #001d35;
+  opacity: 8%;
 }
 
 </style>
