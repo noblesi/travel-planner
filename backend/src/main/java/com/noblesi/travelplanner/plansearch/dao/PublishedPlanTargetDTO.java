c41dbb6c3638ac462d6ba2b04d0a4c9aa15ec0f9
devckddhks <dev.ckddhks@gmail.com>
2026-08-12 17:40:10 +0900

    fix: spring mybatis 로 변경

--- backend/src/main/java/com/noblesi/travelplanner/plansearch/dao/PublishedPlanTargetDTO.java
+++ backend/src/main/java/com/noblesi/travelplanner/plansearch/dao/PublishedPlanTargetDTO.java
@@ -1,8 +1,11 @@
 package com.noblesi.travelplanner.plansearch.dao;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Alias("PublishedPlanTargetDTO")
 @Getter
 @Setter
 public class PublishedPlanTargetDTO {
--- backend/src/main/java/com/noblesi/travelplanner/plansearch/dto/PlanListResponseDTO.java
+++ backend/src/main/java/com/noblesi/travelplanner/plansearch/dto/PlanListResponseDTO.java
@@ -1,8 +1,11 @@
 package com.noblesi.travelplanner.plansearch.dto;
 
+import org.apache.ibatis.type.Alias;
+
 import lombok.Getter;
 import lombok.Setter;
 
+@Alias("PlanListResponseDTO")
 @Getter
 @Setter
 public class PlanListResponseDTO {
